197c5a550fe30e2ec5be34855af4ea6d488af6bf
toshoajibade <toshoajibade@gmail.com>
2018-11-08 02:11:42 +0100

    commit update

---
+++ layouts/error.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="error-page">
+    <p class="four-oh-four">404</p>
+    <p>An error has occured, please click the link below to redirect to the home page</p>
+    <div>
+      <nuxt-link to="/">
+        <p class="home-link">HOME</p>
+      </nuxt-link>
+    </div>
+  </div>
+</template>
+
+<script>
+import TheNavbar from '@/components/TheNavbar'
+export default {
+  components: {
+    TheNavbar
+  },
+  layout: 'errorlayout'
+}
+</script>
+<style lang="scss" scoped>
+.error-page {
+  display: flex;
+  height: 100vh;
+  align-items: center;
+  flex-direction: column;
+  & > .four-oh-four {
+    font-size: 10rem;
+  }
+}
+.home-link {
+  color: #76a6ff;
+  font-size: 2rem;
+}
+</style>
+
+
---
+++ layouts/errorlayout.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="body">
+    <TheNavbar />
+    <nuxt class="page-body"/>
+  </div>
+</template>
+
+<script>
+import TheNavbar from '@/components/TheNavbar'
+export default {
+  components: {
+    TheNavbar
+  }
+}
+</script>
+
