bd56e914ce8c7f3047a9fb28b78223237a19f3a3
toshoajibade <toshoajibade@gmail.com>
2018-11-12 21:58:49 +0100

    another fix

--- pages/blog/_blogUrl.vue
+++ pages/blog/_blogUrl.vue
@@ -25,8 +25,8 @@ export default {
 
   /**If the user is navigating from other parts of the site, search the database by blogId but if the user is navigating from a url, extract the last part of the url and use it to query the database */
 
-  async asyncData({ params, req }) {
-    let url = params.blogUrl || req.path.split('/').pop()
+  async asyncData({ route }) {
+    let url = route.path.split('/').pop()
     try {
       let res = await client.getEntries({
         content_type: 'author',
